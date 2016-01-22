8a21c1c5ae95a5f33c3f2344435f34292bf44a7f
sylvaindurand <sylvaindurand@users.noreply.github.com>
2016-01-22 20:06:57 +0100

    Copy svg images

--- make.py
+++ make.py
@@ -1,9 +1,19 @@
 
-# Downloading and extracting Emojione Toolkit archive
+import os, shutil, urllib.request, zipfile
 
-import os, urllib.request, zipfile
+# Create temporary and output folders
 
 tmp = "tmp"
+out = "output"
+
+if not os.path.exists(tmp):
+	os.makedirs(tmp)
+
+if not os.path.exists(out):
+	os.makedirs(out)
+
+# Download and extract Emojione Toolkit archive
+
 url = "https://github.com/Ranks/emojione/archive/master.zip"
 zip = os.path.join(tmp, "master.zip")
 
@@ -15,3 +25,12 @@ if not os.path.isfile(zip):
 
 with zipfile.ZipFile(zip, "r") as z:
     z.extractall(tmp)
+
+# Copy images
+
+src = os.path.join(tmp, "emojione-master", "assets", "svg")
+
+for svg in os.listdir(src):
+    svg = os.path.join(src, svg)
+    if (os.path.isfile(svg)):
+        shutil.copy(svg, out)
