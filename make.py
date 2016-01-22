af651071cb835068f994a296062a3b659cfe45bf
sylvaindurand <sylvaindurand@users.noreply.github.com>
2016-01-22 23:38:14 +0100

    Generation of phpBB pack

--- make.py
+++ make.py
@@ -1,16 +1,17 @@
 
-import os, shutil, urllib.request, zipfile
+import os, shutil, urllib.request, zipfile, json
 
-# Create temporary and output folders
+# Create temporary and imgput folders
 
 tmp = "tmp"
-out = "output"
+img = "emojione"
 
 if not os.path.exists(tmp):
 	os.makedirs(tmp)
 
-if not os.path.exists(out):
-	os.makedirs(out)
+if not os.path.exists(img):
+	os.makedirs(img)
+
 
 # Download and extract Emojione Toolkit archive
 
@@ -26,11 +27,39 @@ if not os.path.isfile(zip):
 with zipfile.ZipFile(zip, "r") as z:
     z.extractall(tmp)
 
-# Copy images
+
+# Copy SVG
 
 src = os.path.join(tmp, "emojione-master", "assets", "svg")
 
 for svg in os.listdir(src):
     svg = os.path.join(src, svg)
     if (os.path.isfile(svg)):
-        shutil.copy(svg, out)
+        shutil.copy(svg, img)
+
+
+# Read JSON
+
+src = os.path.join(tmp, "emojione-master", "emoji.json")
+
+n = 20 # Number of smilies shown
+k = 0
+
+d = json.loads(open(src).read())
+
+f = open('emojione.pak', 'w')
+
+def export(path, name, code):
+	global k
+	k = k+1
+	code = code.replace("\\","\\\\")  # sanitize strings
+	code = code.replace("'","\'")
+	f.write("'"+img+"/"+path+".svg',	'20',	'20',	'"+str(1*(k<n))+"',	'"+name+"',	'"+code+"',\n")
+
+for i in sorted(d.items(), key=lambda x: int(x[1]['emoji_order'])):
+	for j in i[1]['aliases_ascii']+i[1]['aliases']+[i[1]['shortname']]:
+		name = (i[1]['name'][:40] + '...') if len(i[1]['name']) > 40 else i[1]['name']
+		code = (j[:40] + j[-2:]) if len(j) > 40 else j
+		export(i[1]['unicode'], name, code)
+
+f.close()
