9e5c4825882bba806a0d58720aa6012d64747cea
sylvaindurand <sylvaindurand@users.noreply.github.com>
2016-01-23 08:57:45 +0100

    Count smilies and better comments

--- make.py
+++ make.py
@@ -1,7 +1,9 @@
 
 import os, shutil, urllib.request, zipfile, json
 
+
 # Create temporary and imgput folders
+# -----------------------------------
 
 tmp = "tmp"
 img = "emojione"
@@ -14,6 +16,7 @@ if not os.path.exists(img):
 
 
 # Download and extract Emojione Toolkit archive
+# ---------------------------------------------
 
 url = "https://github.com/Ranks/emojione/archive/master.zip"
 zip = os.path.join(tmp, "master.zip")
@@ -28,7 +31,8 @@ with zipfile.ZipFile(zip, "r") as z:
     z.extractall(tmp)
 
 
-# Copy SVG
+# Copy SVG files
+# --------------
 
 src = os.path.join(tmp, "emojione-master", "assets", "svg")
 
@@ -38,28 +42,36 @@ for svg in os.listdir(src):
         shutil.copy(svg, img)
 
 
-# Read JSON
+# Read JSON and export in PAK format
+# ----------------------------------
 
 src = os.path.join(tmp, "emojione-master", "emoji.json")
 
 n = 20 # Number of smilies shown
-k = 0
+k = 0  # Iteration
 
 d = json.loads(open(src).read())
 
 f = open('emojione.pak', 'w')
 
-def export(path, name, code):
-	global k
-	k = k+1
-	code = code.replace("\\","\\\\")  # sanitize strings
-	code = code.replace("'","\'")
-	f.write("'"+img+"/"+path+".svg',	'20',	'20',	'"+str(1*(k<n))+"',	'"+name+"',	'"+code+"',\n")
-
+# Loop for each smiley
 for i in sorted(d.items(), key=lambda x: int(x[1]['emoji_order'])):
+
+	# Count smilies
+	k = k+1
+
+	# Loop for each smiley shortcut
 	for j in i[1]['aliases_ascii']+i[1]['aliases']+[i[1]['shortname']]:
+
+		# Drop if length exceeds 40 characters
 		name = (i[1]['name'][:40] + '...') if len(i[1]['name']) > 40 else i[1]['name']
 		code = (j[:40] + j[-2:]) if len(j) > 40 else j
-		export(i[1]['unicode'], name, code)
+
+		# Sanitize strings
+		code = code.replace("\\","\\\\")
+		code = code.replace("'","\'")
+
+		# Write in file
+		f.write("'"+img+"/"+i[1]['unicode']+".svg',	'20',	'20',	'"+str(1*(k<n))+"',	'"+name+"',	'"+code+"',\n")
 
 f.close()
